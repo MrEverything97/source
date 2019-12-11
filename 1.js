1d46d2d5246df4dd2c50de4b67c1d10d4925afed
nampp1204 <nampp1204@gmail.com>
2019-12-11 14:03:37 +0700

    add all

--- 1.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContLoaded",function(){
-    var nut=document.getElementsByClassName('nut');
-    var sua=document.getElementsByClassName('sua');
-    nut.onclick=function(){
-        sua[0].classList.add('an');
-    }
-},false)
